c3024b587cf033e0939c8965676a1c20e52b99a3
linuxwt <tengwanginit@gmail.com>
2019-01-11 03:56:45 +0800

    提交文件

---
+++ tomcat_web/jdk.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u192-b12/750e1c8617c5452694857ad95c3ee230/jdk-8u192-linux-x64.tar.gz
+
